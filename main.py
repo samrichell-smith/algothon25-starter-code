56283b2897dae8e6ec70ff191ae69d80f1ba0d31
Sam Richell-Smith <samrs6012@gmail.com>
2025-06-26 16:38:26 +1200

    Implement backtester and initial strategy

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-
 import numpy as np
 
-##### TODO #########################################
-### IMPLEMENT 'getMyPosition' FUNCTION #############
-### TO RUN, RUN 'eval.py' ##########################
-
 nInst = 50
-currentPos = np.zeros(nInst)
-
+maxDollarPosition = 10000
 
 def getMyPosition(prcSoFar):
-    global currentPos
-    (nins, nt) = prcSoFar.shape
-    if (nt < 2):
-        return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
-    lNorm = np.sqrt(lastRet.dot(lastRet))
-    lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
-    currentPos = np.array([int(x) for x in currentPos+rpos])
-    return currentPos
+    nInst, nDays = prcSoFar.shape
+    positions = np.zeros(nInst, dtype=int)
+    breakout_window = 20
+
+    #no change for first 20 days as this is the data we are getting signals from
+    if nDays < breakout_window + 1:
+        return positions
+
+
+    for i in range(nInst):
+        recent_prices = prcSoFar[i, -breakout_window-1:-1]
+        price_today = prcSoFar[i, -1]
+
+        high = np.max(recent_prices)
+        low = np.min(recent_prices)
+
+
+        #if current price is higher or lower than max or min in last 20 days, buy/put maximum amount
+        if price_today > high:
+            num_shares = int(maxDollarPosition / price_today)
+            positions[i] = num_shares
+        elif price_today < low:
+            num_shares = int(maxDollarPosition / price_today)
+            positions[i] = -num_shares
+        else:
+            positions[i] = 0
+
+    return positions
